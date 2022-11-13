fb7500355e940f88a55eb104ac54dd83c8e0ba24
RusakovNikki <rusakov.nikki@yandex.ru>
2022-11-13 17:39:12 +0300

    добавлена возможность указывать роль и аватарку

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
+
+import { auth, db, uploadData, useAuth } from "../firebase"
+import { ROLES } from "../utils/consts"
+
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { doc, setDoc } from "firebase/firestore"
 
 const Login = () => {
-  function createNewUser(e) {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [roleUser, setRole] = useState(ROLES.EMPLOYEE)
+  const [avatar, setAvatar] = useState(null)
+
+  // const currentUser = useAuth()
+  // console.log(currentUser)
+
+  async function createNewUser(e) {
     e.preventDefault()
+
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    )
+    const user = await userCredential.user
+    console.log(user)
+
+    const about = await setDoc(doc(db, "aboutUser", user.uid), {
+      role: roleUser,
+    })
+    console.log(about)
+
+    const upload = uploadData(avatar, user)
+    console.log(upload)
   }
 
   return (
@@ -10,20 +40,37 @@ const Login = () => {
       <form onSubmit={createNewUser}>
         <label htmlFor="role">Кем вы являетесь?</label>
         <br />
-        <select name="role" id="role">
-          <option value="employee">Я сотрубник компании</option>
-          <option value="supervisor">Я являюсь руководителем команды</option>
+        <select name="role" id="role" onChange={(e) => setRole(e.target.value)}>
+          <option value={ROLES.EMPLOYEE}>Я сотрубник компании</option>
+          <option value={ROLES.SUPERVISOR}>
+            Я являюсь руководителем команды
+          </option>
         </select>
         <br />
-        <input type="text" placeholder="Введите никнейм" name="userName" />{" "}
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Введите почту"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <br />
         <input
+          value={password}
           type="password"
           placeholder="Введите пароль"
           name="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
         />{" "}
         <br />
-        {/* <input type="file" name="logo" accept="image/*" id="" /> */}
+        <input
+          type="file"
+          name="logo"
+          accept="image/*"
+          onChange={(e) => setAvatar(e.target.files[0])}
+        />
         <br />
         <input type="submit" value="Создать аккаунт" />
       </form>
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
+import { getStorage, ref, uploadBytes } from "firebase/storage"
+import { useEffect, useState } from "react"
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCx6diEsZEg1-JVpRFQKZYjbLvEc6xXWHM",
@@ -9,10 +12,30 @@ const firebaseConfig = {
     projectId: "jamboo-hrm",
     storageBucket: "jamboo-hrm.appspot.com",
     messagingSenderId: "879411737975",
-    appId: "1:879411737975:web:819e738a32a89c26ef9d72"
+    appId: "1:879411737975:web:819e738a32a89c26ef9d72",
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+const storage = getStorage()
+
+// Custom Hook
+
+export function useAuth() {
+    const [currentUser, setCurrentUser] = useState()
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, user => setCurrentUser(user))
+        return unsub
+    }, [])
+
+    return currentUser
+}
+
+export async function uploadData(file, currentUser) {
+    const fileRef = ref(storage, `avatars/${currentUser.uid}.png`)
+
+    const snapshot = await uploadBytes(fileRef, file)
+}
