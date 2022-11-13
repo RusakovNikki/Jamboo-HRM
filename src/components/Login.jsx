db602d50eab8924282234aed4b8f53e503abe060
RusakovNikki <rusakov.nikki@yandex.ru>
2022-11-13 14:04:48 +0300

    начало создания пользователя

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,31 @@
 import React from "react"
 
 const Login = () => {
+  function createNewUser(e) {
+    e.preventDefault()
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={createNewUser}>
         <label htmlFor="role">Кем вы являетесь?</label>
         <br />
         <select name="role" id="role">
           <option value="employee">Я сотрубник компании</option>
-          <option value="employee">Я являюсь руководителем команды</option>
+          <option value="supervisor">Я являюсь руководителем команды</option>
         </select>
         <br />
-        <input type="text" placeholder="Введите никнейм" name="login" /> <br />
+        <input type="text" placeholder="Введите никнейм" name="userName" />{" "}
+        <br />
         <input
           type="password"
           placeholder="Введите пароль"
           name="password"
         />{" "}
         <br />
-        <input type="file" name="logo" accept="image/*" id="" />
+        {/* <input type="file" name="logo" accept="image/*" id="" /> */}
         <br />
-        <button>Создать аккаунт</button>
+        <input type="submit" value="Создать аккаунт" />
       </form>
     </div>
   )
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCx6diEsZEg1-JVpRFQKZYjbLvEc6xXWHM",
@@ -11,7 +10,9 @@ const firebaseConfig = {
     storageBucket: "jamboo-hrm.appspot.com",
     messagingSenderId: "879411737975",
     appId: "1:879411737975:web:819e738a32a89c26ef9d72"
-};
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
