22da45055c2b1c6985eaa1c79088255847c32a5f
RusakovNikki <rusakov.nikki@yandex.ru>
2023-02-23 20:18:01 +0300

    add calendar

--- src/components/CalendarPage.jsx
+++ src/components/CalendarPage.jsx
@@ -1,15 +1,41 @@
-import React from "react"
+import React, { useContext, useState } from "react"
 import FullCalendar from "@fullcalendar/react"
+// import { Calendar } from "@fullcalendar/core"
 import dayGridPlugin from "@fullcalendar/daygrid"
+import multiMonthPlugin from "@fullcalendar/multimonth"
 import esLocale from "@fullcalendar/core/locales/ru"
 
+import { Context } from "../context"
+
 const CalendarPage = () => {
+    let { currentUserData, currentCompany, setCurrentCompany } =
+        useContext(Context)
+    const [dates, setDates] = useState([])
+    console.log(dates)
+    useState(() => {
+        if (currentCompany) {
+            let copyCompany = JSON.parse(JSON.stringify(currentCompany))
+            copyCompany?.statuses?.forEach((status) => {
+                status.tasks.forEach((task) => {
+                    dates.push({
+                        title: task.text,
+                        start: task.dateStart,
+                        end: task.dateEnd,
+                    })
+                })
+            })
+            setDates(dates)
+        }
+        console.log(dates)
+    }, [])
+
     return (
         <FullCalendar
             locale={esLocale}
             defaultView="dayGridMonth"
-            plugins={[dayGridPlugin]}
-            events={[{ title: "My birthday", date: "2023-02-24" }]}
+            plugins={[multiMonthPlugin, dayGridPlugin]}
+            events={dates}
+            initialView="multiMonthYear"
         />
     )
 }
